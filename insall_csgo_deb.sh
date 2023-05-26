a91aa29eee3b03af6dde2e1be4e1678b3f693a4d
Norbert István Péter <peter.norbert.istvan@gmail.com>
2023-05-26 14:33:46 +0200

    Update insall_csgo_deb.sh

--- insall_csgo_deb.sh
+++ insall_csgo_deb.sh
@@ -1,7 +1,7 @@
 #CS Go SRV ubuntu 20.04 LTS +
 #
 #Install dependencies x86 stuff needed
-sudo dpkg --add-architecture i386; sudo apt update; sudo apt install curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc-s1 lib32stdc++6 libsdl2-2.0-0:i386 steamcmd
+sudo dpkg --add-architecture i386; sudo apt update; sudo apt install curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc-s1 lib32stdc++6 libsdl2-2.0-0:i386 steamcmd -y
 #csgoserver user create
 adduser csgoserver
 #change to csgoserver user
